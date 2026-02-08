d6ec240123866cabc62f0bab4c26e07cdf95b693
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-08 17:38:37 +0530

    Time: 445 ms (81.95%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,7 +1,4 @@
-SELECT 
-    activity_date AS day,
-    COUNT(DISTINCT user_id) AS active_users 
-FROM Activity
-WHERE 
-    activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
-GROUP BY activity_date
+SELECT activity_date as day, COUNT(distinct user_id) AS active_users
+FROM Activity 
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date 
