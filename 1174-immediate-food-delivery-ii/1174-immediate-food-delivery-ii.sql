1c5e5e4103f846f801976261633dae02bcec53b0
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-08 12:47:24 +0530

    Time: 527 ms (98.85%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0))* 100 / COUNT(DISTINCT customer_id), 2) AS immediate_percentage
+FROM Delivery
+WHERE(customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date) 
+    FROM Delivery 
+    GROUP BY customer_id
+);
