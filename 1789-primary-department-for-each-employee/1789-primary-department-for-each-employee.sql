8c33e72d691b659949285fe48a341b3468c9a8f1
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-10 15:03:33 +0530

    Time: 492 ms (84.37%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT employee_id, department_id
+FROM Employee
+WHERE employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(employee_id) = 1 
+) 
+OR primary_flag = 'Y'
