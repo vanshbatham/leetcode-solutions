15cff49eacb7d9558dc8f8acfe9abfdafbe8e1ac
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-11 23:18:40 +0530

    Time: 1357 ms (31.95%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,18 @@
+(SELECT name AS results
+FROM Users
+INNER JOIN MovieRating USING(user_id)
+GROUP BY user_id
+ORDER BY COUNT(rating) DESC, name
+LIMIT 1
+)
+
+UNION ALL
+
+(SELECT title as results
+FROM Movies
+INNER JOIN MovieRating USING(movie_id)
+WHERE month(created_at) = '02' AND year(created_at) = '2020'
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1
+)
