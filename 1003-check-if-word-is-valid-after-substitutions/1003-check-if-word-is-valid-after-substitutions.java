93d7a023db1a49fa1af868f83355600dd3c39dff
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-14 11:00:23 +0530

    Time: 9 ms (83.82%), Space: 46.6 MB (39.22%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == 'c'){
+                if(stack.size() < 2) return false;
+                if(stack.pop() != 'b') return false;
+                if(stack.pop() != 'a') return false;
+            }else{
+                stack.push(c);
+            }
+        }
+    return stack.isEmpty();
+    }
+}
