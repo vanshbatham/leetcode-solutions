7c2c7f253415a73323619df41f0f931aa1971352
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-16 07:09:56 +0530

    Time: 126 ms (96.44%), Space: 107.9 MB (96.1%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -0,0 +1,65 @@
+class TimeStampedValue{
+    public int timestamp;
+    public String value;
+
+    public TimeStampedValue(int timestamp, String value){
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+}
+
+class TimeMap {
+    Map<String, ArrayList<TimeStampedValue>> entriesByKey;
+
+    public TimeMap() {
+        entriesByKey = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!entriesByKey.containsKey(key)){
+            entriesByKey.put(key, new ArrayList<>());
+        }
+        ArrayList<TimeStampedValue> timeStampedValues = entriesByKey.get(key);
+        timeStampedValues.add(new TimeStampedValue(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!entriesByKey.containsKey(key)) return "";
+        ArrayList<TimeStampedValue> timeStampedValues = entriesByKey.get(key);
+        Optional<TimeStampedValue> timeStamp = binarySearchTimestamp(
+            timeStampedValues, timestamp);
+        if(timeStamp.isEmpty()){
+            return "";
+        }
+    return timeStamp.get().value;    
+    }
+
+    private Optional<TimeStampedValue> binarySearchTimestamp(
+        ArrayList<TimeStampedValue> arr, int target){
+        int left = 0, right = arr.size() - 1;
+        int matchIndex = -1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            TimeStampedValue curr = arr.get(mid);
+            if(curr.timestamp <= target){
+                matchIndex = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        if(matchIndex == -1){
+            return Optional.empty();
+        }
+    return Optional.of(arr.get(matchIndex));
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
