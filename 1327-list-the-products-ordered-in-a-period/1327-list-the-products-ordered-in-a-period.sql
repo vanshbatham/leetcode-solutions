b1f39d42c68cf18583a321c71c0a3c9e6f4de775
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-18 10:30:05 +0530

    Time: 763 ms (27.8%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,5 @@
+SELECT product_name, SUM(unit) AS unit
+FROM Products INNER JOIN Orders USING (product_id)
+WHERE MONTH(order_date) = 2 AND YEAR(order_date) = 2020
+GROUP BY product_name
+HAVING unit >= 100;
