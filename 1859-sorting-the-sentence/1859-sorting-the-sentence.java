f1b3ef7b177c24241fe6f63e8375dde9a67f97d5
Vansh Batham <vanshbatham.pro@gmail.com>
2026-02-25 12:40:02 +0530

    Time: 1 ms (44.63%), Space: 43 MB (28.72%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String sortSentence(String s) {
+        Map<Integer, String> map = new HashMap<>();
+          
+        //split words
+        for(String word : s.split(" ")){
+            int lastIdx = word.length() - 1;
+            
+            //get word and index separately
+            int idx = word.charAt(lastIdx) - '0';
+            String actualWord = word.substring(0, lastIdx);
+
+            map.put(idx, actualWord);
+        }
+        
+        //build actual string
+        StringBuilder res = new StringBuilder();
+        for(Map.Entry<Integer, String> indexWord : map.entrySet()){
+            res.append(indexWord.getValue());
+            res.append(" ");
+        }
+        return res.toString().trim();
+    }
+}
